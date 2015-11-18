d2e0507be4cb367c43dfca3a9409f0ad46141e7b
Guest User <Guest@stk-cs01-ws12.westlothian.sch.uk>
2015-11-18 13:33:17 +0000

    Sprint 1.0

---
+++ PUPIL.java
@@ -0,0 +1,52 @@
+import javax.swing.JOptionPane;
+
+public class PUPIL
+{
+    private String fName;
+    private String sName;
+    private int mark;
+    
+    public PUPIL()
+    {
+      fName = "";
+      sName = "";
+      mark =  0;
+      setUpStudentList();
+      studentFile = new FILEREADCSV();
+      
+      
+      public void setUpStudentList()
+      {
+        
+        
+        
+        
+        }
+      
+      
+    
+    
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
